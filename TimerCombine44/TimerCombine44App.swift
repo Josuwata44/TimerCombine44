34ae8dbcee309967502c7a5fd87bc841597a7a69
Josua44 <103715118+Josuwata44@users.noreply.github.com>
2022-07-18 11:44:46 +0900

    Initial Commit

---
+++ TimerCombine44/TimerCombine44App.swift
@@ -0,0 +1,17 @@
+//
+//  TimerCombine44App.swift
+//  TimerCombine44
+//
+//  Created by cmStudent on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct TimerCombine44App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
